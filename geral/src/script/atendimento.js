ff54cb8766821f4ea91f83305f850a4b15222a6f
Gustavo <gustavopv159@gmail.com>
2023-10-03 21:03:45 -0300

    init commit

---
+++ geral/src/script/atendimento.js
@@ -0,0 +1,10 @@
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/651c9d36e6bed319d0059731/1hbrsc82i';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
